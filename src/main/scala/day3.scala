b9c3c5049a067b1ff9fc51fd528ae3929ba22535
ChidiRNweke <chidi125@gmail.com>
2023-12-04 18:32:48 +0100

    Finish part 2 of day 3

--- src/main/scala/day3.scala
+++ src/main/scala/day3.scala
@@ -3,22 +3,25 @@
 //> using file utils/utilities.scala
 //> using dep org.typelevel::cats-parse::1.0.0
 //> using dep org.typelevel::cats-core::2.10.0
+package day3
 
 import utils.readFile
 import cats.parse.Numbers.{digits}
 import cats.parse.Parser
+import Nums._
 
 case class Symbol(value: Char, r: Int, c: Int)
 case class Nums(value: Int, adjacent: List[Symbol])
 
 object Nums:
+  import ParseInput._
   def fromRowColumn(
-      rawValue: String,
+      rawInput: String,
       row: Int,
       col: Int,
       symbols: List[Symbol]
   ) =
-    val colRange = (col - rawValue.length() + 1 to col)
+    val colRange = (col - rawInput.length() + 1 to col)
 
     def isAdjacent(col: Int)(r: Int, c: Int): Boolean =
       (row == r && math.abs(col - c) == 1)
@@ -32,7 +35,14 @@ object Nums:
 
     val h = symbols.filter { case Symbol(_, r, c) => isAdjacentAny(r, c) }
     val diag = symbols.filter { case Symbol(_, r, c) => isDiagonal(r, c) }
-    Nums(rawValue.toInt, h ::: diag)
+    Nums(rawInput.toInt, (h ::: diag).distinct)
+
+  def rawInputToNums(rawInput: String, symbols: List[Symbol]) =
+    numberParser
+      .parseAll(rawInput)
+      .getOrElse(throw new Exception("failed to parse"))
+      .map((c, caret) => Nums.fromRowColumn(c, caret.line, caret.col, symbols))
+      .filter(_.adjacent.nonEmpty)
 
 object Symbol:
   def symbolsFromParseResult(input: String): List[Symbol] =
@@ -56,28 +66,21 @@ object ParseInput:
 
 @main def day3: Unit =
   import Symbol._
-  import ParseInput._
-  val example = """467..114..
-                  |...*......
-                  |..35..633.
-                  |......#...
-                  |617*......
-                  |.....+.58.
-                  |..592.....
-                  |......755.
-                  |...$.*....
-                  |.664.598..""".stripMargin
 
   val input = readFile("day3.txt")
     .getOrElse(throw new Exception("Failed to read file"))
 
   val symbols = symbolsFromParseResult(input)
-  val solution1 = numberParser
-    .parseAll(input)
-    .getOrElse(throw new Exception("failed to parse"))
-    .map((c, caret) => Nums.fromRowColumn(c, caret.line, caret.col, symbols))
-    .filter(_.adjacent.nonEmpty)
-    .map(_.value)
-    .sum
+  val solution1 = rawInputToNums(input, symbols).map(_.value).sum
+
+  val gears = symbols.filter(_.value == '*')
+  val solution2 =
+    rawInputToNums(input, gears)
+      .groupBy(_.adjacent)
+      .map(_._2)
+      .filter(_.length >= 2)
+      .map(_.foldRight(1)(_.value * _))
+      .sum
 
   println(solution1)
+  println(solution2)
