878e50b8e0ddff6311a4927e4643e501e3f8fe56
ChidiRNweke <chidi125@gmail.com>
2023-12-05 06:53:29 +0100

    Finish day 4 part 2

--- src/main/scala/day4.scala
+++ src/main/scala/day4.scala
@@ -1,25 +1,25 @@
-// scala-cli directives
 //> using resourceDir ../resources
 //> using file utils/utilities.scala
 //> using dep org.typelevel::cats-parse::1.0.0
 //> using dep org.typelevel::cats-core::2.10.0
 package day4
 
-case class Hand(as: List[Int])
-case class Match(left: Hand, right: Hand):
-  private val matches = left.as.intersect(right.as).length
-  val points = if matches == 0 then 0 else math.pow(2, matches - 1)
-
+import cats.syntax.all._
+import cats.Semigroup
 import utils.readFile
 import cats.parse.Numbers.{digits}
 import cats.parse.Parser
 import cats.parse.Rfc5234.{wsp, alpha, crlf}
+
+case class Hand(as: List[Int])
+case class Match(left: Hand, right: Hand):
+  val matches = left.as.intersect(right.as).length
+  val points = if matches == 0 then 0 else math.pow(2, matches - 1)
+
 object ParseInput:
   private val card = alpha.rep ~ wsp.rep ~ digits ~ Parser.char(':')
   private val sep = Parser.char('|').void
-
   private val any = Parser.anyChar
-
   private val hand = digits
     .map(_.toInt)
     .surroundedBy(wsp.rep.?)
@@ -27,17 +27,21 @@ object ParseInput:
     .map(c => Hand(c.toList))
 
   private val parseHand = hand.between(card, sep)
-
   val parseGame = (parseHand ~ hand).repSep(any).map(_.map(Match(_, _)))
 
-@main def day4: Unit =
+def duplicateCards(seen: Map[Int, Int], cards: (Match, Int)): Map[Int, Int] =
+  val (round, cardnr) = cards
+  val timesSeen = seen.get(cardnr + 1)
+  val won = (cardnr + 2 to round.matches + cardnr + 1).toList
 
-  val example = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-                  |Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-                  |Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-                  |Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-                  |Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-                  |Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11""".stripMargin
+  val result = timesSeen match
+    case None        => cardnr + 1 :: won
+    case Some(value) => cardnr + 1 :: List.fill(value + 1)(won).flatten
+
+  val resultMap = result.groupBy(identity).view.mapValues(_.size).toMap
+  Semigroup[Map[Int, Int]].combine(seen, resultMap)
+
+@main def day4: Unit =
 
   val input = readFile("day4.txt")
     .getOrElse(throw new Exception("Failed to read file"))
@@ -45,5 +49,11 @@ object ParseInput:
   val parsed = ParseInput.parseGame
     .parseAll(input)
     .getOrElse(throw new Exception("Parsing failed"))
-  val solution = parsed.map(_.points).toList.sum
-  println(solution)
+
+  val solution1 = parsed.map(_.points).sum
+
+  val solution2 =
+    parsed.zipWithIndex.foldLeft(Map.empty)(duplicateCards).values.sum
+
+  println(solution1)
+  println(solution2)
